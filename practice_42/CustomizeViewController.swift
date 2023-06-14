32799e4548980b893629ed65a7f41ab8cedf42a2
Nvtsaruk <nvtsaruk@gmail.com>
2023-06-14 14:52:50 +0300

    UI creation

---
+++ practice_42/CustomizeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomizeViewController.swift
+//  practice_42
+//
+//  Created by Tsaruk Nick on 14.06.23.
+//
+
+import UIKit
+
+class CustomizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
